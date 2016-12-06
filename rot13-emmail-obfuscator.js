17c316895f79271bb1ddeedb02ea2498bb3f23e2
dustin1pollock <dustin.pollock@mohawkcollege.ca>
2016-12-06 15:16:18 -0500

    Updating The Script File WIth Better Comments

--- rot13-emmail-obfuscator.js
+++ rot13-emmail-obfuscator.js
@@ -5,12 +5,13 @@ function str_rot13 (str) {
         })
 }
 
+//Replace Email Address In Quotes
 var email = str_rot13("ebgngvba13@grfgvat.pbz");
 
-//Jquery example
+//Jquery Placement Statement
 $(".email-jquery").attr("href", "mailto:"+email).html(email);
 
-//Raw Javascript
+//Raw Javascript Placement Statement
 var x = document.getElementsByClassName("email-java");
 for(i=0;i<x.length;i++){
     x[i].innerHTML = email;
